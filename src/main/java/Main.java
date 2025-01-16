4b0c83830ef8a54febe77a0b8195fef496a5e254
“BinhPham1110” <binhgold1110@gmail.com>
2025-01-16 18:28:56 +0700

    Bai tap Pham Thanh Binh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        // TODO: Khai báo biến và viết chương trình theo yêu cầu trong README.md
+        // Khai báo và gán giá trị cho các biến
+        int intValue = 42;
+        double doubleValue = 3.14;
+        String stringValue = "Hello, Java!";
 
-        // Ví dụ:
-        // int myInt = 42;
-        // double myDouble = 3.14;
-        // String myString = "Hello, Java!";
-        // System.out.println(myInt);
-        // System.out.println(myDouble);
-        // System.out.println(myString);
+        // In giá trị của các biến ra màn hình
+        System.out.println("Giá trị của int: " + intValue);
+        System.out.println("Giá trị của double: " + doubleValue);
+        System.out.println("Giá trị của String: " + stringValue);
+
+        // Kiểm tra giá trị của biến int
+        if (intValue > 10) {
+            System.out.println("Greater than 10");
+        } else {
+            System.out.println("Less or equal to 10");
+        }
     }
 }
